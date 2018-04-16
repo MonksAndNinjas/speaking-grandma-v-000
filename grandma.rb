63b4783dd8221ca491eb65417f809929474d5d5d
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-16 17:59:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  if phrase.upcase == true
+  if phrase.upcase == true && phrase != "I LOVE YOU GRANDMA!"
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
